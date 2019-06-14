62e09a9ad3388c1bb3a241ac14ef35139994d5da
AyatBaghoush <44270741+AyatBaghoush@users.noreply.github.com>
2019-06-14 08:45:27 +0300

    Update testBase class to support json file reading

--- app/src/androidTest/java/com/mytaxi/android_demo/TestUtils/BaseTestRobot.java
+++ app/src/androidTest/java/com/mytaxi/android_demo/TestUtils/BaseTestRobot.java
@@ -1,15 +1,24 @@
 package com.mytaxi.android_demo.TestUtils;
 
 
+import android.content.res.Resources;
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.espresso.contrib.DrawerActions;
 import android.support.test.rule.ActivityTestRule;
+import android.support.test.rule.GrantPermissionRule;
 import android.test.ActivityInstrumentationTestCase2;
+import android.util.JsonReader;
 import android.view.Gravity;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.mytaxi.android_demo.DataProvider.JsonData;
 import com.mytaxi.android_demo.R;
+import com.mytaxi.android_demo.Robots.LoginRobot;
+import com.mytaxi.android_demo.Robots.SearchRobot;
 import com.mytaxi.android_demo.activities.MainActivity;
 
+import org.junit.Before;
 import org.junit.Rule;
 
 import static android.support.test.espresso.Espresso.onView;
@@ -25,8 +34,14 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static android.support.test.espresso.assertion.ViewAssertions.* ;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 
+import com.mytaxi.android_demo.DataProvider.JsonFileReader;
+
+
 public class BaseTestRobot extends ActivityInstrumentationTestCase2<MainActivity> {
 
+    public JsonData jsonDataReader;
+    Gson gson = new Gson();
+    JsonObject jsonObject;
 
     public BaseTestRobot()
     {
@@ -37,12 +52,24 @@ public class BaseTestRobot extends ActivityInstrumentationTestCase2<MainActivity
     public ActivityTestRule<MainActivity> intentsRule =
             new ActivityTestRule<>(MainActivity.class);
 
+    @Rule
+    public GrantPermissionRule permissionRule = GrantPermissionRule.grant(android.Manifest.permission.ACCESS_FINE_LOCATION);
+    public Resources resources;
 
     @Override
     protected void setUp() throws Exception
     {
         super.setUp();
         getActivity();
+    }
+
+
+    @Before
+    public void init() {
+
+        resources = intentsRule.getActivity().getResources();
+        jsonObject = JsonFileReader.getJSONResource(resources, R.raw.data );
+        jsonDataReader = gson.fromJson(jsonObject, JsonData.class);
     }
 
     public static void checkTextIsDisplayed(ViewInteraction ve , String expectedTitle )
